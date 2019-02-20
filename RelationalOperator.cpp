83238fa1509e5847403d575c014f2dcaf68a6a98
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2019-02-21 01:56:03 +0600

    Add files via upload

---
+++ RelationalOperator.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int t;
+
+int main(){
+
+            cin>>t;
+
+            int a,b;
+
+            while(t--){
+
+                            cin>>a>>b;
+
+                            if(a>b) cout<<">"<<endl;
+
+                            else if(a<b) cout<<"<"<<endl;
+
+                            else cout<<"="<<endl;
+
+
+            }
+
+
+
+return 0;
+}
