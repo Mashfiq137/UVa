b21a6b5f567e23591b8f6967712408476e7a4a57
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2019-02-21 21:23:11 +0600

    Add files via upload

---
+++ SearchingForNessy.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int t;
+
+int main(){
+
+            cin>>t;
+
+            int n,m;
+
+            while(t--){
+
+                        cin>>n>>m;
+
+                        cout<<(n/3)*(m/3)<<endl;
+
+            }
+
+}
