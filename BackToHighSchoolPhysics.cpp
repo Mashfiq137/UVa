0d6f9ee30c03cd4ae65d68174a9da3c50c1210db
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2019-03-10 00:32:24 +0600

    Add files via upload

---
+++ BackToHighSchoolPhysics.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+            int v,t;
+
+            while(scanf("%d %d",&v,&t)!=EOF)
+            {
+
+                cout<<(v*2*t)<<endl;
+
+            }
+
+
+return 0;
+}
