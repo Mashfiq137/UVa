309e96545f9adea6a8b44a0579f98b7b373494ef
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2019-03-14 05:09:53 +0600

    Add files via upload

---
+++ Cola.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+            int n;
+
+            while(scanf("%d",&n)==1)
+            {
+
+                cout<<n + n/2<<endl;
+
+            }
+
+
+return 0;
+}
