40709ecbfcdef0c7315c223c7df87723d7cf8b2b
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2019-03-18 02:18:05 +0600

    Add files via upload

---
+++ BafanaBafana.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int cs = 0;
+int main(){
+
+            int n,p,k;
+
+            int t;
+
+            cin>>t;
+
+            while(t--)
+            {
+            int cnt=0;
+
+            cin>>n>>k>>p;
+
+            k--;
+
+            printf("Case %d: %d\n",++cs,(k+p)%n+1);
+
+            }
+
+return 0;
+}
